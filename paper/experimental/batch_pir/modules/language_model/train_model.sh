44599d7b6b0e0f577d0ce3f95817ea51d248632e
myl7 <myl@myl.moe>
2024-10-28 18:23:55 +0800

    Remove unmatching files after forking

--- paper/experimental/batch_pir/modules/language_model/train_model.sh
+++
@@ -1,2 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.  
-python main.py --cuda --emsize 650 --nhid 650 --dropout 0.5 --epochs 40 --tied --cuda
--- paper/experimental/batch_pir/setup.sh
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.  
-
-export PYTHONPATH=$PYTHONPATH:`pwd`/modules/language_model/
-export PYTHONPATH=$PYTHONPATH:`pwd`/modules/taobao_rec/
-export PYTHONPATH=$PYTHONPATH:`pwd`/modules/movielens_rec/
