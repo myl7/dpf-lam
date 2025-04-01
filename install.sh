2fba29d87f539a5e5a78a85ca6f380c62bd3e372
myl7 <myl@myl.moe>
2025-04-01 15:41:02 +0800

    Setup project and build

--- install.sh
+++
@@ -1,2 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
-CC=g++ python setup.py install
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
 
-from setuptools import setup, Extension
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import CUDAExtension
 from torch.utils import cpp_extension
 
-setup(name='dpf_cpp',
-      ext_modules=[
-          CUDAExtension('dpf_cpp', sources=[
-              'dpf_wrapper.cu',
-          ], extra_compile_args=['-std=c++17'],
-          )
-      ],
-      cmdclass={'build_ext': cpp_extension.BuildExtension},
+setup(
+    name="dpf_cpp",
+    packages=find_packages(include=["dpf_gpu"]),
+    ext_modules=[
+        CUDAExtension(
+            "dpf_cpp",
+            sources=[
+                "dpf_wrapper.cu",
+            ],
+            extra_compile_args=["-std=c++17"],
+        )
+    ],
+    cmdclass={"build_ext": cpp_extension.BuildExtension},
 )
-
-
